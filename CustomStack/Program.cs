835d354191f0cb917a4aea3169e578b97ba645fd
Mahdie Ghorbani <GhorbaniMahdie0@gmail.com>
2023-08-10 19:07:06 +0330

    Valid Parentheses

---
+++ CustomStack/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var Stack = new CustomStack<int>();
+            var Stack2 = new CustomStack<string>();
+            
+            Validator validator = new Validator();
+            validator.IsValid(new char[] { '(', '{', '}', ')' });
+            //validator.IsValid(new char[] { '(', '[', ']', '}', '{', ')' });
+        }
+    }
+}
---
+++ CustomStack/Validator.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomStack
+{
+    public class Validator
+    {
+        public bool IsValid(Char[] chars)
+        {
+            CustomStack<char> stack = new CustomStack<char>();
+            char topOfStack = '0';
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (stack.HasItem() == true)
+                topOfStack= stack.Peek();
+
+                if ((chars[i]==')' && topOfStack=='(')
+                    || (chars[i] == ']' && topOfStack == '[')
+                    || (chars[i] == '}' && topOfStack == '{'))
+                    stack.Pop();
+                else
+                    stack.Push(chars[i]);
+            }
+            if (stack.HasItem())
+                return false;
+            else 
+               return true;
+        }
+    }
+}
